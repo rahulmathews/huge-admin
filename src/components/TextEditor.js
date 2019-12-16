fdbf2b415f7173b31353f3ca311162a7a6464a4c
Rahul <rahul@bigappcompany.in>
2019-12-16 09:10:45 +0530

    Added Text Editor

---
+++ src/components/TextEditor.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+import { EditorState, convertToRaw } from 'draft-js'
+import { Editor } from 'react-draft-wysiwyg';
+
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import '../styles/components/editor.css';
+
+class TextEditor extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            editorState: EditorState.createEmpty(),
+        }
+    }
+
+    componentDidMount = () => {
+        const elems = document.getElementsByClassName('rdw-editor-main');
+
+        for(let ele of elems){
+            ele.style.height = "200px";
+            ele.style.padding = "4px";
+            ele.style.border = "2px solid #17b7b79e";
+            ele.style['border-radius'] = "5px";
+        }
+    }
+
+    render(){
+        return (
+            <Editor
+                toolbar={{
+                    image: { alt: { present: true }, previewImage: false },
+                    fontFamily: {
+                    options: ['Arial', 'Georgia', 'Impact', 'Tahoma', 'Roboto', 'Times New Roman', 'Verdana'],
+                    }
+                }}
+                placeholder={this.props.placeholder}
+                hashtag={{}}
+            />
+        )
+    }
+}
+
+export default TextEditor;
--- src/pages/section/AddSection.js
+++ src/pages/section/AddSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     Button,
     Card,
@@ -20,86 +20,141 @@ import {
 import * as _ from 'lodash';
 import styled from 'styled-components';
 import {SketchPicker} from 'react-color';
+import { EditorState, convertToRaw } from 'draft-js'
+import { Editor } from 'react-draft-wysiwyg';
 
 import Page from 'components/Page';
+import TextEditor from 'components/TextEditor';
 
 const SubmitButtonStyles = styled.div`
   margin-top : 30px
 `
 
-const AddSection = () => {
-    return(
-        <Page className="add-section" breadcrumbs={[{ name: 'section', active: false }, { name: 'add', active: true }]}>
-            <Card>
-                <CardHeader className="card-header">Add Section</CardHeader>
-                <CardBody>
-                    <Row>
-                        <Col xl={4} lg={12} md={12}>
+class AddSection extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount = () => {
+        
+    }
+
+    render(){
+        return(
+            <Page className="add-section" breadcrumbs={[{ name: 'section', active: false }, { name: 'add', active: true }]}>
+                <Card>
+                    <CardHeader className="card-header">Add Section</CardHeader>
+                    <CardBody>
+                        <Row>
+                            <Col xl={4} lg={12} md={12}>
+                                <Form>
+                                    <FormGroup>
+                                        <Label for="title">Name</Label>
+                                        <Input
+                                            placeholder="Enter Name"
+                                            id="title"
+                                        />
+                                    </FormGroup>
+                                </Form>
+                            </Col>
+                            <Col xl={4} lg={12} md={12}>
+                                <Form>
+                                    <FormGroup>
+                                        <Label for="font-size">Font Size</Label>
+                                        <Input
+                                            placeholder="Enter Font Size"
+                                            id="font-size"
+                                            type="select"
+                                        >
+                                            {_.range(2, 40, 2).map((key, idx) => {
+                                                return (
+                                                    <option key={idx}>{key}</option>
+                                                )
+                                            })}
+                                        </Input>
+                                    </FormGroup>
+                                </Form>
+                            </Col>
+                            <Col xl={4} lg={12} md={12}>
+                                <Form>
+                                    <FormGroup>
+                                        <Label for="font-color">Font Color</Label>
+                                        <Input
+                                            placeholder="Select Font Color"
+                                            id="font-color"
+                                            type="color"
+                                        />
+                                        {/* <SketchPicker /> */}
+                                    </FormGroup>
+                                </Form>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xl={4} lg={12} md={12}>
+                                <Form>
+                                    <FormGroup>
+                                        <Label for="bg-color">Background Color</Label>
+                                        <Input
+                                            placeholder="Select Background Color"
+                                            id="bg-color"
+                                            type="color"
+                                        />
+                                    </FormGroup>
+                                </Form>
+                            </Col>
+                            <Col xl={4} lg={12} md={12}>
+                                <Form>
+                                    <FormGroup>
+                                        <Label for="bg-image">Background Image</Label>
+                                        <Input
+                                            id="bg-image"
+                                            type="file"
+                                        />
+                                    </FormGroup>
+                                </Form>
+                            </Col>
+                            <Col xl={4} lg={12} md={12}>
+                                <Form>
+                                    <FormGroup>
+                                            
+                                    </FormGroup>
+                                </Form>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xl={12} lg={12} md={12}>
                             <Form>
                                 <FormGroup>
-                                    <Label for="title">Name</Label>
-                                    <Input
-                                        placeholder="Enter Name"
-                                        id="title"
-                                    />
+                                    <Label for="paragraph">Paragraph</Label>
+                                        <TextEditor placeholder="Enter Paragraph"/>
                                 </FormGroup>
                             </Form>
-                        </Col>
-                        <Col xl={4} lg={12} md={12}>
-                            <Form>
-                                <FormGroup>
-                                    <Label for="font-size">Font Size</Label>
-                                    <Input
-                                        placeholder="Enter Font Size"
-                                        id="font-size"
-                                        type="select"
-                                    >
-                                        {_.range(2, 40, 2).map((key, idx) => {
-                                            return (
-                                                <option key={idx}>{key}</option>
-                                            )
-                                        })}
-                                    </Input>
-                                </FormGroup>
-                            </Form>
-                        </Col>
-                        <Col xl={4} lg={12} md={12}>
-                            <Form>
-                                <FormGroup>
-                                    <Label for="font-color">Font Color</Label>
-                                    <Input
-                                        placeholder="Select Font Color"
-                                        id="font-color"
-                                        type="color"
-                                    />
-                                    {/* <SketchPicker /> */}
-                                </FormGroup>
-                            </Form>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xl={4} lg={12} md={12}>
-                            <Form>
-                                <FormGroup>
-                                    <Label for="bg-color">Background Color</Label>
-                                    <Input
-                                        placeholder="Select Background Color"
-                                        id="bg-color"
-                                        type="color"
-                                    />
-                                </FormGroup>
-                            </Form>
-                        </Col>
-                        <Col xl={4} lg={12} md={12}>
-                            <SubmitButtonStyles>
-                                <Button>Submit</Button>
-                            </SubmitButtonStyles>
-                        </Col>
-                    </Row>
-                </CardBody>
-            </Card>
-        </Page>
-    )
+                            </Col>
+                        </Row>
+                        {/* <TextEditor /> */}
+                        <Row>
+                            <Col xl={12} lg={12} md={12}>
+                                <Form>
+                                    <FormGroup>
+                                        <Label for="title">Title</Label>
+                                        <TextEditor placeholder="Enter Title"/>
+                                    </FormGroup>
+                                </Form>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xl={4} lg={12} md={12}>
+                                <SubmitButtonStyles>
+                                    <Button>Submit</Button>
+                                </SubmitButtonStyles>
+                            </Col>
+                        </Row>
+                        
+                    </CardBody>
+                </Card>
+            </Page>
+        )
+    }
 }
 
 export default AddSection
